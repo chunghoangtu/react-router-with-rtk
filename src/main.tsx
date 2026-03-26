c4fa4fa4e6345fd620604dcb0ce705bbb06b6a82
ChungHoangTu <chunghoangtu@gmail.com>
2026-03-26 22:16:21 +0700

    feat: re structure  app folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppWithDeclarativeModeRouter from "@/AppWithDeclarativeModeRouter";
+
+// import AppWithDeclarativeModeRouter from "@/app/declarative/AppWithDeclarativeModeRouter";
+import AppWithDataModeRouter from "@/app/data/AppWithDataModeRouter";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppWithDeclarativeModeRouter />
+    {/* <AppWithDeclarativeModeRouter /> */}
+    <AppWithDataModeRouter />
   </StrictMode>,
 );
